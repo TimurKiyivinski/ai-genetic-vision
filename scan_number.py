fa233daa9cb6f004dad880135791fcf52a1c0c1c
Timur Kiyivinski <deathseclipse@gmail.com>
2015-05-27 22:36:09 +0800

    Final

--- scan_number.py
+++ scan_number.py
@@ -2,7 +2,6 @@
 import os
 import sys
 import png
-import glob
 import copy
 import random
 import argparse
@@ -21,26 +20,74 @@ RECRATE = 10
 
 # Class to contain the X and Y coordinates
 class Coordinates:
+    '''
+    Constructor
+
+    x           Int:        X coordinate
+    y           Int:        Y coordinate
+
+    self.x      X coordinate
+    self.y      Y coordinate
+    '''
     def __init__(self, x, y):
         self.x = x
         self.y = y
+    '''
+    Coordinates.get
+
+    returns:    Int, Int
+    '''
     def get(self):
         return self.x, self.y
 
 # Main PNG Bitmap class, stores all the bitmaps
 class PNGMap:
+    '''
+    Constructor
+    
+    PNGName     String:     Name of the PNG
+    PNGArray    List:       2D list bitmap
+    PNGWhite    Int:        Background colour bitmap
+
+    self.name    Name of the bitmap
+    self.bitmap  A 2D list containing the image as a bitmap
+    self.white   The background colour of a bitmap
+    self.dim     The bitmap dimension
+    self.size    The bitmap area
+    '''
     def __init__(self, PNGName, PNGArray = [[]], PNGWhite = 1):
         self.name = PNGName
         self.bitmap = PNGArray
         self.white = PNGWhite
         self.dim = len(PNGArray)
         self.size = len(PNGArray) * len(PNGArray[0])
+    '''
+    PNGMap.inverse
+
+    Returns the inverse token of the token at a given location
+    
+    bit         Int:        Value of token to inverse
+
+    returns:    Void
+    '''
     def inverse(self, bit):
         if bit == self.white:
             return 0
         else:
             return self.white
+    '''
+    PNGMap.surrounded
+    
+    Checks if a given map coordinate is surrounded
+    
+    x           Int:        X coordinate
+    y           Int:        Y coordinate
+
+    returns:    Boolean
+    '''
     def surrounded(self, x, y):
+        # Check all borders of a token and see if they are
+        # not a wall
         sim = 0
         if x > 0 and y in range(0, self.dim):
             if self.bitmap[x - 1][y] != self.white:
@@ -54,14 +101,32 @@ class PNGMap:
         if y < len(self.bitmap[0]) - 1 and x in range(0, self.dim):
             if self.bitmap[x][y + 1] != self.white:
                 sim += 1
+        # A token is surrounded if there are 4 other tokens around it
         return sim == 4
+    '''
+    PNGMap.line
+
+    Squashes the 2D bitmap into one line
+    
+    returns:    List
+    '''
     def line(self):
         bitmapLine = []
         for row in self.bitmap:
             bitmapLine += row
         return bitmapLine
+    '''
+    PNGMap.breed
+
+    Creates a hybrid of both PNGMaps
+
+    partnerMap  PNGMap:     PNGMap to breed with
+
+    returns:    PNGMap
+    '''
     def breed(self, partnerMap):
         babyArray = []
+        # Split the two bitmaps at a random location and combine them
         splitLine = random.randrange(1, self.dim)
         for i in range(0, splitLine):
             babyArray.append(self.bitmap[i])
@@ -69,50 +134,17 @@ class PNGMap:
             babyArray.append(partnerMap.bitmap[i])
         babyMap = PNGMap(self.name, babyArray)
         return babyMap
-    def lmost(self):
-        leftMost = self.dim
-        for row in self.bitmap:
-            for i in range(0, self.dim):
-                if row[i] != self.white:
-                    if i < leftMost:
-                        leftMost = i
-        return leftMost
-    def rmost(self):
-        rightMost = 0
-        for row in self.bitmap:
-            for i in range(0, self.dim):
-                if row[i] != self.white:
-                    if i > rightMost:
-                        rightMost = i
-        return rightMost
-    def umost(self):
-        for i in range(0, self.dim):
-            for ii in range(0, self.dim):
-                if self.bitmap[i][ii] != self.white:
-                    return i
-    def dmost(self):
-        downMost = 0
-        for i in range(0, self.dim):
-            for ii in range(0, self.dim):
-                if self.bitmap[i][ii] != self.white:
-                    if i > downMost:
-                        downMost = i
-        return downMost
-    def zone(self):
-        zoneBitmap = self.bitmap
-        for i in range(self.umost(), self.dmost()):
-            for ii in range(self.lmost(), self.rmost() + 1):
-                if zoneBitmap[i][ii] != self.white:
-                    zoneBitmap[i][ii] = self.white
-                    break
-            for ii in reversed(range(self.lmost(), self.rmost() + 1)):
-                if zoneBitmap[i][ii] != self.white:
-                    zoneBitmap[i][ii] = self.white
-                    break
-    # Mutates the current bitmap, vital part of a genetic algorithm
+    '''
+    PNGMap.mutate
+
+    Slightly mutates the image bitmap
+
+    returns:    Void
+    '''
     def mutate(self):
-        # need to improve this for user detection.. most probable culprit
+        # Select a mutation type, and run them based on their weight
         mutationType = random.randrange(0, 10)
+        # This mutation adds random artifacts
         if mutationType < 6:
             charCo = []
             for i in range(0, self.dim):
@@ -165,6 +197,7 @@ class PNGMap:
                             added = True
                         else:
                             continue
+        # This mutation stretches a bitmap
         elif mutationType > 8:
             noAdds = random.randrange(1, ADDRATE)
             for row in self.bitmap:
@@ -173,8 +206,13 @@ class PNGMap:
                         if row[i + 1] == self.white:
                             row[i + 1] = row[i]
                             break
-    # Checks how similar another bitmap is compared
-    # to the current bitmap
+    '''
+    PNGMap.like
+
+    Compares a PNGMap with another
+
+    returns:    Float
+    '''
     def like(self, comparePNG):
         if self.size != comparePNG.size:
             return 0
@@ -189,28 +227,63 @@ class PNGMap:
                         similarities += 1
                     encounters += 1
         return float(float(similarities) / float(encounters))
+    '''
+    PNGMap.similar
+
+    Compares a PNGMap with an entire list of PNGMaps
+
+    returns:    Float
+    '''
     def similar(self, comparePNGs):
         return sum(float(self.like(comparePNG)) for comparePNG in comparePNGs)
         
-#TODO: This function may or may not find a partner
+'''
+randPair
+
+Fitness Function: Gets a weighted random PNGMap based on it's similarity
+
+PNGMaps         List:       List of maps to select from 
+compareMaps     List:       List of maps to compare to
+totalFitness    Int:        Maximum fitness value
+threadQueue     Processor.Queue
+
+returns:        PNGMap
+'''
 def randPair(PNGMaps, compareMaps, totalFitness, threadQueue):
+    # Create an evenly distributed random number
     roulette = random.uniform(0, totalFitness)
+    # Iterate list until a solution is found, by testing similarity
+    # This helps to create a weighted random and eliminate less
+    # fit parents
     for listMap in PNGMaps:
         roulette -= listMap.similar(compareMaps)
         if roulette <= 0:
             threadQueue.put(listMap)
             return
-    print('Error')
-    return PNGMaps[random.randint(0, len(PNGMaps) - 1)]
+    # As a last resort, return a non weighted parent
+    return PNGMaps[random.randrange(0, len(PNGMaps))]
 
+'''
+genPairs
+
+Parent Selection Function: Creates a set of parents to breed
+
+PNGMaps         List:       List of maps to select from
+compareMaps     List:       List of maps to compare to
+
+returns:        List, List
+'''
 def genPairs(PNGMaps, compareMaps):
     pairA = []
     pairB = []
+    # Maximum possible fitness
     totalFitness = len(compareMaps)
     threadsA = []
     threadsB = []
+    # Thread safe way to get parent PNGMaps
     queueA = Queue()
     queueB = Queue()
+    # Create a list of threads to get a PNGMap
     for listMap in PNGMaps:
         threadA = Process(target=randPair , args=(PNGMaps, compareMaps, totalFitness, queueA))
         threadB = Process(target=randPair , args=(PNGMaps, compareMaps, totalFitness, queueB))
@@ -218,49 +291,77 @@ def genPairs(PNGMaps, compareMaps):
         threadB.start()
         threadsA.append(threadA)
         threadsB.append(threadB)
+    # Get the parents from the queues
     while not len(pairA) == len(PNGMaps):
         pairA.append(queueA.get())
     while not len(pairB) == len(PNGMaps):
         pairB.append(queueB.get())
+    # Join the threads with the current one
     for thread in threadsA:
         thread.join()
     for thread in threadsB:
         thread.join()
+    # Return the pair of PNGMaps
     return pairA, pairB
 
+'''
+evolutionGen
+
+Evolution Function: Manages the selection of the best outcome,
+                    evolution of the next generation and mutations
+                    of them
+
+userMutations   List:       List of user PNGMap mutations
+resourceBitmaps List:       List of resource PNGMaps
+recursionDepth  Int:        Depth of recursive calls, number of generations
+bestMap         PNGMap:     Copy of best PNGMap
+resMap          PNGMap:     Copy of best resource PNGMap
+
+returns:        PNGMap, PNGMap
+'''
 def evolutionGen(userMutations, resourceBitmaps, recursionDepth, bestMap, resMap = PNGMap('Default')):
     recursionDepth += 1
     print('Currently at generation: %i' % recursionDepth)
+    # Iterate list of PNGMaps and set the best as the current detected solution
     for userChild in userMutations:
         finalRes = False
         for resourceBitmap in resourceBitmaps:
-            #if userChild.like(resourceBitmap) > bestMap.like(resMap):
-            #    print('Type 1')
-            #    bestMap = copy.deepcopy(userChild)
-            #    resMap = resourceBitmap
-            #if resourceBitmap.like(userChild) > resMap.like(bestMap):
-            #    print('Type 2')
-            #    bestMap = copy.deepcopy(userChild)
-            #    resMap = resourceBitmap
             if userChild.like(resourceBitmap) + resourceBitmap.like(userChild) > bestMap.like(resMap) + resMap.like(bestMap):
                 bestMap = copy.deepcopy(userChild)
                 resMap = resourceBitmap
     print('Current comparison is: %f' % bestMap.like(resMap))
+    # Return the best if their similarity is past the SIMRATE
     if bestMap.like(resMap) >= SIMRATE:
         return bestMap, resMap
+    # Recursively call this function until RECRATE is reached
     if recursionDepth < RECRATE:
+        # Create pairs of parents based on their fitness
         parentsA, parentsB = genPairs(userMutations, resourceBitmaps)
+        # Iterate the parents and create children by breeding them
         newChildren = []
         for i in range(0, len(parentsA)):
             newChild = copy.deepcopy(parentsA[i].breed(parentsB[i]))
+            # Mutate a few children, in hope that X-Men as awesome as
+            # Wolverine and Rogue may be created. 
             if random.randint(0, 10) % MUTRATE >= 1:
                 newChild.mutate()
             newChildren.append(newChild)
+        # Recusively call this function with the new set of children, by returning it
         return evolutionGen(newChildren, resourceBitmaps, recursionDepth, bestMap, resMap)
     else:
+        # As a last resort, return the best solutions detected until now
         print('Evolution ended at generation: %i' % recursionDepth)
         return bestMap, resMap
 
+'''
+getPNGArray
+
+Creates a 2D bitmap from an image file
+
+bitmapFile      String:     Path to iamge file
+
+returns:        List
+'''
 def getPNGArray(bitmapFile):
     userFile = open(bitmapFile, 'rb')
     userPNG = png.Reader(userFile)
@@ -273,6 +374,15 @@ def getPNGArray(bitmapFile):
         arrRow += 1
     return bitmapArr
 
+'''
+getPNGResource
+
+Loads all the resources from the resource directory
+
+resourceDir     String:     Directory to laod comparison bitmaps
+
+returns:        List
+'''
 def getPNGResource(resourceDir):
     PNGList = []
     resourceList = os.listdir(resourceDir)
@@ -282,12 +392,20 @@ def getPNGResource(resourceDir):
         PNGList.append(PNGBitmap)
     return PNGList
 
+'''
+printPNGArray
+
+Prints the entire bitmap
+
+returns:        Void
+'''
 def printPNGArray(bitmapArr):
     for row in bitmapArr:
         for bit in row:
             sys.stdout.write(str(bit))
         print('')
 
+# Main function
 def main(args):
     # Read the command line arguments and set default values
     setVerbose = args.verbose
@@ -297,12 +415,16 @@ def main(args):
     saveDir = args.save
     mutateCount = int(args.mutate)
     print('Reading bitmap %s' % bitmapFile)
+    # Load the user bitmap
     bitmapArr = getPNGArray(bitmapFile)
+    # Create a user PNGMap
     userMap = PNGMap('User', bitmapArr)
+    
     # Compare Operation
     if compareFile != '':
         compareArr = getPNGArray(compareFile)
         compareMap = PNGMap('Compare', compareArr)
+        # Optionally create user mutations
         for i in range(0, mutateCount):
             userMap.mutate()
         if setVerbose:
@@ -313,6 +435,7 @@ def main(args):
         print('Similarity between %s and %s is:' % (userMap.name, compareMap.name))
         print(userMap.like(compareMap))
         return
+    
     # Normal operation
     # Load all the resources into an array
     resourcePNG = getPNGResource(resourceDir)
@@ -323,6 +446,7 @@ def main(args):
         for resourceBitmap in resourcePNG:
             print('Resource file (%s):' % resourceBitmap.name)
             printPNGArray(resourceBitmap.bitmap)
+    # Create a list of user mutations
     userMutations = []
     userMutations.append(copy.deepcopy(userMap))
     for i in range(0, GENRATE - 1):
@@ -334,6 +458,7 @@ def main(args):
         if setVerbose:
             printPNGArray(mutationNext.bitmap)
     print('Initial similarity is: %f' % userMap.similar(resourcePNG))
+    # Get the final user mutation and detected value
     finalUser, finalResource = evolutionGen(userMutations, resourcePNG, 0, userMap)
     # Results
     print('The detected value is: %s' % finalResource.name)
@@ -342,12 +467,15 @@ def main(args):
     print('Final comparison is: %f' % finalUser.like(finalResource))
     if setVerbose:
         printPNGArray(finalUser.bitmap)
+    # Optionally save the last user mutation into a bitmap
     if saveDir != False:
         saveImage = png.from_array(finalUser.bitmap, 'L;1')
         saveFile = open(saveDir, 'wb')
         saveImage.save(saveFile)
 
+# Run main if not loaded as a module
 if __name__ == '__main__':
+    # Create a parser to handle command line arguments
     parser = argparse.ArgumentParser(description='Converts a numerical bitmap into text.')
     parser.add_argument('-f', '--file', help='Bitmap file name', required=True)
     parser.add_argument('-c', '--compare', help='Compare only a single file', default='', required=False)
@@ -356,4 +484,5 @@ if __name__ == '__main__':
     parser.add_argument('-s', '--save', help='Save solution image', default=False, required=False)
     parser.add_argument('-v', '--verbose', help='Verbose logging', action='store_true')
     args = parser.parse_args()
+    # Call the main
     quit(main(args))
